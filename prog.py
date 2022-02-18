15508bc00221044435da60b08b69716229d79456
Aelie54 <klein.amelie1991@gmail.com>
2022-02-18 09:56:17 +0100

    ok

--- prog.py
+++ prog.py
@@ -4,7 +4,6 @@ server = smtplib.SMTP_SSL('smtp-pawolanmwen.alwaydata.net', 465)
 server.login("sandbox@loryleticee.com", "myqTem-0dejnu-wytwaw")
 server.sendmail( 
     "sandbox@loryleticee.com", 
-    "myqTem-0dejnu-wytwaw"info@loryleticee.fr",
-    "subject: Nouvelle image dispo sur le serveur \n Ce message vient de python. Blip blop STOP."
-    )
+    "info@loryleticee.fr",
+    "subject: Nouvelle image dispo sur le serveur \n Ce message vient de python. Blip blop STOP.")
 server.quit()
